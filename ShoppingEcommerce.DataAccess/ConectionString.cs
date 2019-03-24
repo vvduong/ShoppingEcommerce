a4f98a6c8c70c6271de74e425be265ca0d7f0cda
VuDuong <VuDuong@DESKTOP-AFMFCGO>
2019-03-24 12:47:19 +0700

    add utilitil

---
+++ ShoppingEcommerce.DataAccess/ConectionString.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Web;
+using LacViet.ShoppingEcommerce.Utilities;
+
+namespace LacViet.ShoppingEcommerce.DataAccess
+{
+    public class SingleConnection
+    {
+        private SingleConnection() { }
+        private static SingleConnection _ConsString = null;
+        private String _String = null;
+        private ShoppingEcommercePrincipal _SurePrinpal = null;
+        private static string _connectionStringDefault = ConfigurationManager.ConnectionStrings["ShoppingEcommerceContext"].ConnectionString;
+        public static string ConString
+        {
+            get
+            {
+                if (HttpContext.Current.User is ShoppingEcommercePrincipal)
+                {
+                    var userCurrent = HttpContext.Current.User as ShoppingEcommercePrincipal;
+                    //if (_ConsString == null)
+                    //{
+                    _ConsString = new SingleConnection
+                    {
+                        _String = SingleConnection.Connect(userCurrent.ServerName, userCurrent.DBName, userCurrent.LoginDB, userCurrent.Password)
+                    };
+                    return _ConsString._String;
+                    //}
+                    //else
+                    //    return _ConsString._String;
+                }
+                else
+                {
+                    return _connectionStringDefault;
+                }
+
+            }
+        }
+
+        public static string Connect(string dataSource, string initialCatalog, string userID, string password)
+        {
+            //Build an SQL connection string
+            SqlConnectionStringBuilder sqlString = new SqlConnectionStringBuilder()
+            {
+                DataSource = dataSource, // Server name
+                InitialCatalog = initialCatalog,  //Database
+                UserID = userID,         //Username
+                Password = password,  //Password
+                MultipleActiveResultSets = true,
+                PersistSecurityInfo = true,
+            };
+
+            //Build an Entity Framework connection string
+            EntityConnectionStringBuilder entityString = new EntityConnectionStringBuilder()
+            {
+                Provider = "System.Data.SqlClient",
+                Metadata = @"res://*/ShoppingEcommerceContext.csdl|
+                            res://*/ShoppingEcommerceContext.ssdl|
+                            res://*/ShoppingEcommerceContext.msl",
+                ProviderConnectionString = sqlString.ToString()
+            };
+            return entityString.ConnectionString;
+        }
+    }
+}
---
+++ ShoppingEcommerce.Utilities/ShoppingEcommercePrincipal.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacViet.ShoppingEcommerce.Utilities
+{
+
+    interface IShoppingEcommercePrincipal : IPrincipal
+    {
+        Guid ID { get; set; }
+        string LoginName { get; set; }
+        string LanguageCulture { get; set; }
+        bool IsAdmin { get; set; }
+        Guid ApplicationID { get; set; }
+        string ServerName { get; set; }
+        string DBName { get; set; }
+        string LoginDB { get; set; }
+        string Password { get; set; }
+        List<string> MembershipRole { get; set; }
+    }
+    public class ShoppingEcommercePrincipal : IShoppingEcommercePrincipal
+    {
+        #region - DECLARE -
+        #endregion
+        #region - PROPERTY -
+        public Guid ID { get; set; }
+        public string LoginName { get; set; }
+        public IIdentity Identity { get; set; }
+        public string LanguageCulture { get; set; }
+        public bool IsAdmin { get; set; }
+        public Guid ApplicationID { get; set; }
+        public List<string> MembershipRole { get; set; }
+        private string serverName;
+        public string ServerName
+        {
+            get
+            {
+                return EncryptionUtils.DecryptText(serverName);
+            }
+            set { serverName = value; }
+        }
+        private string dBName;
+        public string DBName
+        {
+            get
+            {
+                return EncryptionUtils.DecryptText(dBName);
+            }
+            set { dBName = value; }
+        }
+        private string loginDB;
+        public string LoginDB
+        {
+            get
+            {
+                return EncryptionUtils.DecryptText(loginDB);
+            }
+            set { loginDB = value; }
+        }
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return EncryptionUtils.DecryptText(password);
+            }
+            set { password = value; }
+        }
+        public bool IsInRole(string role)
+        {
+
+            return MembershipRole.Where(name=>name.ToString().ToLower()== role).Count()>0;
+            //return this.MembershipUser != null &&this.MembershipUser.MembershipRoles != null && role != null && this.MembershipUser.MembershipRoles.Where(p=>p.RoleName.Equals(role,StringComparison.CurrentCultureIgnoreCase)).Count()>0;
+        }
+        #endregion
+        public ShoppingEcommercePrincipal(string username)
+        {
+            this.Identity = new GenericIdentity(username);
+        }
+    }
+    public class ShoppingEcommercePrincipalSerializeModel
+    {
+        public Guid ID { get; set; }
+        public string LoginName { get; set; }
+        public string LanguageCulture { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public Guid ApplicationID { get; set; }
+        public string ServerName { get; set; }
+        public string DBName { get; set; }
+        public string LoginDB { get; set; }
+        public string Password { get; set; }
+        public List<string> MembershipRole { get; set; }
+    }
+}
