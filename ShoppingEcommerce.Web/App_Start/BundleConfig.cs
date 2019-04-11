a6d3125d7b3b0a19c91c9fa1b510249e38950a25
VuDuong <VuDuong@DESKTOP-AFMFCGO>
2019-04-11 22:30:10 +0700

    inclue css

--- ShoppingEcommerce.Web/App_Start/BundleConfig.cs
+++ ShoppingEcommerce.Web/App_Start/BundleConfig.cs
@@ -29,27 +29,41 @@ namespace ShoppingEcommerce.Web
                   "~/Scripts/jquery.validate*"));
 
 
-      bundles.Add(new StyleBundle("~/bundles/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-      bundles.Add(new StyleBundle("~/Contents/vendor/bootstrap/css").Include(
-                      "~/Contents/vendor/bootstrap/css/bootstrap.min.css"));
-      //bundles.Add(new StyleBundle("~/bundles/css").Include(
-      //              "~/Contents/vendor/bootstrap/css/bootstrap.min.css",
-      //              "~/Contents/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
-      //              "~/Contents/fonts/iconic/css/material-design-iconic-font.min.css",
-      //              "~/Contents/fonts/linearicons-v1.0.0/icon-font.min.css",
-      //              "~/Contents/vendor/animate/animate.css",
-      //              "~/Contents/vendor/css-hamburgers/hamburgers.min.css",
-      //              "~/Contents/vendor/animsition/css/animsition.min.css",
-      //              "~/Contents/vendor/select2/select2.min.css",
-      //              "~/Contents/vendor/daterangepicker/daterangepicker.css",
-      //              "~/Contents/vendor/slick/slick.css",
-      //              "~/Contents/vendor/MagnificPopup/magnific-popup.css",
-      //              "~/Contents/vendor/perfect-scrollbar/perfect-scrollbar.css",
-      //              "~/Contents/css/util.css",
-      //              "~/Contents/css/main.css"
-      //              ));
+      //bundles.Add(new StyleBundle("~/Content/css").Include(
+      //                "~/Content/bootstrap.min.css",
+      //                "~/Content/bootstrap.min.css",
+      //                "~/Content/font-awesome.min.css",
+      //                "~/Content/material-design-iconic-font.min.css",
+      //                "~/Content/icon-font.min.css",
+      //                "~/Content/animate.css",
+      //                "~/Content/hamburgers.min.css",
+      //                "~/Content/animsition.min.css",
+      //                "~/Content/select2.min.css",
+      //                "~/Content/daterangepicker.css",
+      //                "~/Content/slick.css",
+      //                "~/Content/magnific-popup.css",
+      //                "~/Content/perfect-scrollbar.css",
+      //                "~/Content/util.css",
+      //                "~/Content/main.css"
+      //                ));
+      //bundles.Add(new StyleBundle("~/Content/vendor/bootstrap/css").Include(
+      //                "~/Content/vendor/bootstrap/css/bootstrap.min.css"));
+      bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                    "~/Content/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
+                    "~/Content/fonts/iconic/css/material-design-iconic-font.min.css",
+                    "~/Content/fonts/linearicons-v1.0.0/icon-font.min.css",
+                    "~/Content/vendor/animate/animate.css",
+                    "~/Content/vendor/css-hamburgers/hamburgers.min.css",
+                    "~/Content/vendor/animsition/css/animsition.min.css",
+                    "~/Content/vendor/select2/select2.min.css",
+                    "~/Content/vendor/daterangepicker/daterangepicker.css",
+                    "~/Content/vendor/slick/slick.css",
+                    "~/Content/vendor/MagnificPopup/magnific-popup.css",
+                    "~/Content/vendor/perfect-scrollbar/perfect-scrollbar.css",
+                    "~/Content/css/util.css",
+                    "~/Content/css/main.css"
+                    ));
     }
   }
 }
